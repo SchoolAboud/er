9fa39e381922889a0451ccf5aa90ea6fbad422f1
MChalak <63811592+MChalak@users.noreply.github.com>
2020-04-17 13:26:22 -0700

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,35 +1,60 @@
-public class Person {
-   private String name;
-   private Person mother;
-   private Person father;
-   private ArrayList<Person> children;
-   
-   public Person(String name) {//initialize the person (with the first list)
-      this.name = name;
-      this.children = new ArrayList<Person>();
-   }
-      
-   public void setMother(Person mother) {//mutator method for mother
-      this.mother = mother;
-   }
-   
-   public void setFather(Person father) {//mutator method for father
-      this.father = father;
-   }
-   
-   public void addChild(Person child) {//mutator method to add child into ArrayList
-      children.add(child);//adds child to ArrayList children
-      
-   public ArrayList<Person> getChildren() {//accessor method for children
-   return this.children;
-   
-   public Person getMother() {//accessor method for mother
-      return this.mother;
+import java.util.*;
 
-   public Person getFather() {//accessor method for father
-      return this.father;
-      
-   public String getName() {// accessor method for name
-      return name;
-   }
+public class Person {
+
+private ArrayList<String> Person;
+
+
+//This constructor will take a family tree constructed array list.
+public Person (ArrayList<String> p) {
+
+this.Person = p;
+
 }
+
+//This list will contain all mothers' names with recurrences. The index of that list will be the same index of the getChildren() function created list.
+//Hence that will allow us to pair the child with their mother accordingly.
+public ArrayList<String> getMothers() {
+   
+   ArrayList<String> Mothers = new ArrayList<String>();
+   for (int i=1 ; i<Person.size() ; i=i+3) {
+         
+         Mothers.add(Person.get(i));
+     
+         
+   }
+    return Mothers;
+}
+
+
+//This list will contain all fathers' names with recurrences. The index of that list will be the same index of the getChildren() function created list.
+//Hence that will allow us to pair the child with their father accordingly.
+public ArrayList<String> getFathers() {
+   
+   ArrayList<String> Fathers = new ArrayList<String>();
+   for (int i=2 ; i<Person.size() ; i=i+3) {
+         
+         Fathers.add(Person.get(i));
+
+   }
+    return Fathers;
+}
+
+//The following function will filter names. It will create an array list of only the names that have parents.
+//The user will then be asked to pick a name from that list.
+public ArrayList<String> getChildren() {
+   
+   ArrayList<String> Children = new ArrayList<String>();
+   for (int i=0 ; i<Person.size() ; i=i+3) {
+         
+         Children.add(Person.get(i));
+
+   }
+    return Children;
+}
+
+
+}
+
+
+
