1eafd12de5e3236c0d1c92b612dff0424afb323a
rle0 <63812134+rle0@users.noreply.github.com>
2020-04-18 16:40:20 -0700

    Update and rename parents.java to Parents.java
    -added a header block
    -class name changed to be capitalized
    -method and variables changed to start lowercase

--- Parents.java
+++ Parents.java
@@ -1,12 +1,19 @@
+/*CS 211(B)
+Spring 2020
+4/20/2020
+Majd Chalak, Abdalrahman Shaath, Joey Helf, Richard Le
+This file is used to create a family tree with father, mother, and children.
+*/
+
 import java.util.*;
 
-public class parents {
+public class Parents {
 
 private ArrayList<String> parents;
 
 
 //This constructor will take a family tree constructed array list.
-public parents (ArrayList<String> p) {
+public Parents (ArrayList<String> p) {
 
 this.parents = p;
 
@@ -16,14 +23,14 @@ this.parents = p;
 //Hence that will allow us to pair the child with their mother accordingly.
 public ArrayList<String> getMothers() {
    
-   ArrayList<String> Mothers = new ArrayList<String>();
+   ArrayList<String> mothers = new ArrayList<String>();
    for (int i=1 ; i<parents.size() ; i=i+3) {
          
-         Mothers.add(parents.get(i));
+         mothers.add(parents.get(i));
      
          
    }
-    return Mothers;
+    return mothers;
 }
 
 
@@ -31,26 +38,26 @@ public ArrayList<String> getMothers() {
 //Hence that will allow us to pair the child with their father accordingly.
 public ArrayList<String> getFathers() {
    
-   ArrayList<String> Fathers = new ArrayList<String>();
+   ArrayList<String> fathers = new ArrayList<String>();
    for (int i=2 ; i<parents.size() ; i=i+3) {
          
-         Fathers.add(parents.get(i));
+         fathers.add(parents.get(i));
 
    }
-    return Fathers;
+    return fathers;
 }
 
 //The following function will filter names. It will create an array list of only the names that have parents.
 //The user will then be asked to pick a name from that list.
 public ArrayList<String> getChildren() {
    
-   ArrayList<String> Children = new ArrayList<String>();
+   ArrayList<String> children = new ArrayList<String>();
    for (int i=0 ; i<parents.size() ; i=i+3) {
          
-         Children.add(parents.get(i));
+         children.add(parents.get(i));
 
    }
-    return Children;
+    return children;
 }
 
 
