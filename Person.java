fe7ebae956bf41b967cb06eb0d03e5df704abdfd
rle0 <63812134+rle0@users.noreply.github.com>
2020-04-19 14:03:31 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -8,57 +8,55 @@ import java.util.*;
 
 public class Person {
 
-private ArrayList<String> person;
+   private ArrayList<String> person;
 
 
 //This constructor will take a family tree constructed array list.
-public Person (ArrayList<String> p) {
+   public Person (ArrayList<String> p) {
 
-this.person = p;
+      this.person = p;
 
-}
+   }
 
 //This list will contain all mothers' names with recurrences. The index of that list will be the same index of the getChildren() function created list.
 //Hence that will allow us to pair the child with their mother accordingly.
-public ArrayList<String> getMothers() {
+   public ArrayList<String> getMothers() {
    
-   ArrayList<String> mothers = new ArrayList<String>();
-   for (int i=1 ; i<person.size() ; i=i+3) {
+      ArrayList<String> mothers = new ArrayList<String>();
+      for (int i=1 ; i<person.size() ; i=i+3) {
          
-         mothers.add(person.get(i));
-     
+         mothers.add(person.get(i));     
          
-   }
-    return mothers;
-}
+      }
+      return mothers;
+  }
 
 
 //This list will contain all fathers' names with recurrences. The index of that list will be the same index of the getChildren() function created list.
 //Hence that will allow us to pair the child with their father accordingly.
-public ArrayList<String> getFathers() {
+   public ArrayList<String> getFathers() {
    
-   ArrayList<String> fathers = new ArrayList<String>();
-   for (int i=2 ; i<person.size() ; i=i+3) {
+      ArrayList<String> fathers = new ArrayList<String>();
+      for (int i=2 ; i<person.size() ; i=i+3) {
          
          fathers.add(person.get(i));
 
-   }
-    return fathers;
-}
+      }
+      return fathers;
+  }
 
 //The following function will filter names. It will create an array list of only the names that have parents.
 //The user will then be asked to pick a name from that list.
-public ArrayList<String> getChildren() {
+   public ArrayList<String> getChildren() {
    
-   ArrayList<String> children = new ArrayList<String>();
-   for (int i=0 ; i<person.size() ; i=i+3) {
+      ArrayList<String> children = new ArrayList<String>();
+      for (int i=0 ; i<person.size() ; i=i+3) {
          
          children.add(person.get(i));
 
-   }
-    return children;
-}
-
+      }
+      return children;
+  }
 
 }
 
